789ee9c44f84f4ea89f305be92132f13ce5894a2
Morgan <Mharrisstoertz@gmail.com>
2023-04-04 09:24:40 -0400

    created ListView

---
+++ TodoList/Views/ListView.swift
@@ -0,0 +1,30 @@
+//
+//  ListView.swift
+//  TodoList
+//
+//  Created by Morgan Harris-Stoertz on 2023-04-04.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    
+    //MARK: Computed properties
+    var body: some View {
+        
+        NavigationView{
+            VStack{
+                HStack{
+                    
+                }
+            }
+            
+        }
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
