39bba6bdcbd986fb31bd44d476982932bd79bb20
Morgan <Mharrisstoertz@gmail.com>
2023-04-04 09:14:08 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Morgan Harris-Stoertz on 2023-04-04.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
