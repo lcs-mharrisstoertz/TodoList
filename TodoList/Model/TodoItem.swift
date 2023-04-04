4f053ccb5ace0c32c4d236a2aa2ae9c2d82b1405
Morgan <Mharrisstoertz@gmail.com>
2023-04-04 10:00:36 -0400

    added model

--- TodoList/Model/TodoItem.swift
+++ TodoList/Model/TodoItem.swift
@@ -7,3 +7,17 @@
 
 import Foundation
 
+struct TodoItem: Identifiable{
+    var id: Int
+    var description: String
+    var completed: Bool
+}
+
+var existingTodoItems = [
+TodoItem(id: 1, description: "Study for Physics test", completed: false),
+TodoItem(id: 2, description: "Finish Computer Science assignment", completed: true),
+TodoItem(id: 3, description: "Go for a run", completed: false),
+]
+
+
+
